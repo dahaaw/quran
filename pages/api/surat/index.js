5fdc1272736dd0b96b61f431da5ffb61a23b49b4
dahaaw <dahaaw@gmail.com>
2021-09-16 06:31:17 +0700

    try again

--- pages/api/surat/index.js
+++ pages/api/surat/index.js
@@ -2,6 +2,6 @@ const {readFileSync} = require('fs');
 const path = require('path');
 
 export default (req, res) => {
-    const surah = readFileSync(path.join('utils/data/surat.json'));
+    const surah = readFileSync(path.join(__dirname, '..', '..', '..', '..', 'utils/data/surat.json'));
     res.status(200).json(JSON.parse(surah));
 }
