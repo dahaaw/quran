cc16f41e5b9d0b5dbac836a8af54c29f2d318870
dahaaw <dahaaw@gmail.com>
2021-09-16 07:23:36 +0700

    direct to API source

--- pages/api/surat/[id]/[ayat].js
+++ pages/api/surat/[id]/[ayat].js
@@ -5,7 +5,9 @@ const path = require('path');
 
 
 export default async ({query: {id, ayat}}, res) => {
-    const surat = JSON.parse(readFileSync(path.join(__dirname, '/utils/data/surat.json')));
+    // const surat = JSON.parse(readFileSync('./utils/data/surat.json'));
+    const surat = (await axios.get('https://equran.id/api/surat')).data;
+
     const options = {
         includeScore: true,
         keys: ['nomor', 'nama', 'nama_latin', 'arti']
@@ -14,18 +16,17 @@ export default async ({query: {id, ayat}}, res) => {
     const result = fuse.search(id);
     const idSurat = result[0].item.nomor;
 
-    const pathFile = path.join(__dirname, `/utils/data/surat/${idSurat}.js`);
-    if(existsSync(pathFile)){
-        // const dataSurat = readFileSync(pathFile);
-        const dataSurat = await readFile(pathFile);
-        if(dataSurat) return res.status(200).json({success: true, data: JSON.parse(dataSurat)});
-    }
+    // const pathFile = path.join(__dirname, `/utils/data/surat/${idSurat}.js`);
+    // if(existsSync(pathFile)){
+    //     // const dataSurat = readFileSync(pathFile);
+    //     const dataSurat = await readFile(pathFile);
+    //     if(dataSurat) return res.status(200).json({success: true, data: JSON.parse(dataSurat)});
+    // }
 
     axios.get('https://equran.id/api/surat/'+idSurat)
     .then(d => {
-        console.log(d)
         res.status(200).json({success: true, data: d.data});
-        writeFileSync(pathFile, JSON.stringify(d.data));
+        // writeFileSync(pathFile, JSON.stringify(d.data));
     })
     .catch(e => res.status(400).json({success:false, message: 'surat tidak tersedia'}))
 }
--- pages/api/surat/index.js
+++ pages/api/surat/index.js
@@ -1,7 +1,9 @@
 const {readFileSync} = require('fs');
 const path = require('path');
+const axios = require('axios');
 
-export default (req, res) => {
-    const surah = readFileSync(path.join(__dirname, '..', '..', 'utils/data/surat.json'));
-    res.status(200).json(JSON.parse(surah));
+export default async (req, res) => {
+    // const surat = readFileSync(path.join(__dirname, '..', '..', 'utils/data/surat.json'));
+    const surat = (await axios.get('https://equran.id/api/surat')).data;
+    res.status(200).json(surat);
 }
